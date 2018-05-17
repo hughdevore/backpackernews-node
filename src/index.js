5b906ee95ef2b7d726e2a761c724f608e10eeb1c
hughdevore <hughdevore@gmail.com>
2018-05-16 20:43:53 -0600

    Adding post link mutation.

--- src/index.js
+++ src/index.js
@@ -8,17 +8,29 @@ let links = [{
   description: 'GraphQL developer, Backpacker, Sasquatch'
 }]
 
+// Determine the length of the link feed
+let idCount = links.length
+
 // Declare how our GraphQL schema should resolve
 const resolvers = {
   Query: {
     info: () => `This is the GraphQL API of Backpacker News.`,
     feed: () => links,
   },
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url,
-  }
+  Mutation: {
+    post: (root, args) => {
+      // Create the new link to add to the feed
+      const link = {
+        id: `link-${idCount ++}`,
+        description: args.description,
+        url: args.url,
+      }
+      // Add the link to the feed
+      links.push(link)
+      // Return the new link
+      return link
+    }
+  },
 }
 
 // Create our Backpacker News server
