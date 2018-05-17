7042954b9a9468d79531144ff276cc023c72628f
hughdevore <hughdevore@gmail.com>
2018-05-16 20:36:41 -0600

    Moving the GraphQL schema into it's own schema.graphql file.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// Declare our GraphQL Schema
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+
+// Populate the links feed
+let links = [{
+  id: 'link-0',
+  url: 'www.hughdevore.com',
+  description: 'GraphQL developer, Backpacker, Sasquatch'
+}]
 
 // Declare how our GraphQL schema should resolve
 const resolvers = {
   Query: {
-    info: () => `This is the GraphQL API of Backpacker News.`
+    info: () => `This is the GraphQL API of Backpacker News.`,
+    feed: () => links,
+  },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url,
   }
 }
 
 // Create our Backpacker News server
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`The server is running on http::localhost:4000`))
